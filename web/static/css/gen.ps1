bff930016e277510278ac986a11b435bbc7200ef
A.J. Ruckman <aj@ruckman.co>
2019-02-05 10:01:07 -0500

    rearrange CSS

--- web/static/css/gen.ps1
+++ web/static/css/gen.ps1
@@ -2,6 +2,7 @@ ForEach ($scss in $( Get-ChildItem -Exclude theme.scss | Where-Object { $_.Exten
 {
     ForEach ($theme in $( Get-ChildItem -Directory .\themes\ ))
     {
+        Write-Output $($scss.Name) $($theme.Name)
         S:\libsass\sassc.exe -I . -I .\themes\$($theme.Name)\ .\$( $scss.Name ) .\build\$($scss.BaseName).$( $theme.Name ).css
     }
 }
